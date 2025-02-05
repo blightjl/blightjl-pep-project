cb99594001e377b8d38c346e22666e77b158ee89
Josan Limbu <josanlimbu01@gmail.com>
2025-02-05 20:03:46 +0000

    Created DAOs for Message and Account

---
+++ src/main/java/DAO/AccountDAO.java
@@ -0,0 +1,15 @@
+package DAO;
+
+import Model.Account;
+import Util.ConnectionUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * For DAOs, only CR from CRUD should be accessible.
+ */
+public class AccountDAO {
+    
+}
---
+++ src/main/java/DAO/MessageDAO.java
@@ -0,0 +1,15 @@
+package DAO;
+
+import Model.Message;
+import Util.ConnectionUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * For DAOs, only CR from CRUD should be accessible.
+ */
+public class MessageDAO {
+    
+}
