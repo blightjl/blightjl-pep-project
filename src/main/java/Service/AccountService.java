e5c14aac35d14ab7be3ec9f7fee7f720508f958e
Josan Limbu <josanlimbu01@gmail.com>
2025-02-05 20:57:45 +0000

    Outline for Service.

---
+++ src/main/java/Service/AccountService.java
@@ -0,0 +1,11 @@
+package Service;
+
+import DAO.AccountDAO;
+import Model.Account;
+
+/*
+ * Sits between the Controller and the DAO. It calls DAO objects.
+ */
+public class AccountService {
+    
+}
---
+++ src/main/java/Service/MessageService.java
@@ -0,0 +1,11 @@
+package Service;
+
+import DAO.MessageDAO;
+import Model.Message;
+
+/*
+ * Sits between the Controller and the DAO. It calls DAO objects.
+ */
+public class MessageService {
+    
+}
