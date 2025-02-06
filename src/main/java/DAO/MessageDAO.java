1881c88c5ece8d14c90d6c086d1a18d6fcb2fff3
Josan Limbu <josanlimbu01@gmail.com>
2025-02-06 02:41:25 +0000

    Implemented DAO methods

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -11,5 +11,136 @@ import java.util.List;
  * For DAOs, only CR from CRUD should be accessible.
  */
 public class MessageDAO {
-    
+
+    /**
+     * TODO: retrieve all authors from the Author table.
+     * You only need to change the sql String.
+     * @return all Authors.
+     */
+    public List<Message> getAllMessages() {
+        Connection connection = ConnectionUtil.getConnection();
+        List<Message> messages = new ArrayList<>();
+
+        /*
+            create table message (
+                message_id int primary key auto_increment,
+                posted_by int,
+                message_text varchar(255),
+                time_posted_epoch bigint,
+                foreign key (posted_by) references  account(account_id)
+            );
+        */
+
+        try {
+            // SQL logic
+            String sql = "select * from message;";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                Message message = new Message(rs.getInt("message_id"), rs.getInt("posted_by"), rs.getString("message_text"), rs.getLong("time_posted_epoch"));
+                messages.add(message);
+            }
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return messages;
+    }
+
+    /**
+     * TODO: retrieve all authors from the Author table.
+     * You only need to change the sql String.
+     * @return all Authors.
+     */
+    public Message getMessageByID(int message_id) {
+        Connection connection = ConnectionUtil.getConnection();
+        List<Message> messages = new ArrayList<>();
+
+        /*
+            create table message (
+                message_id int primary key auto_increment,
+                posted_by int,
+                message_text varchar(255),
+                time_posted_epoch bigint,
+                foreign key (posted_by) references  account(account_id)
+            );
+        */
+        
+        try {
+            // SQL logic
+            String sql = "select * from message where message_id = ?;";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, message_id);
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next()) {
+                Message message = new Message(rs.getInt("message_id"), rs.getInt("posted_by"), rs.getString("message_text"), rs.getLong("time_posted_epoch"));
+                messages.add(message);
+            }
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     * TODO: retrieve all authors from the Author table.
+     * You only need to change the sql String.
+     * @return all Authors.
+     */
+    public List<Message> getAllMessagesByAccountID(int account_id) {
+        Connection connection = ConnectionUtil.getConnection();
+        List<Message> messages = new ArrayList<>();
+
+        /*
+            create table message (
+                message_id int primary key auto_increment,
+                posted_by int,
+                message_text varchar(255),
+                time_posted_epoch bigint,
+                foreign key (posted_by) references  account(account_id)
+            );
+        */
+
+        try {
+            // SQL logic
+            String sql = "select * from message where posted_by = ?;";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, account_id);
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                Message message = new Message(rs.getInt("message_id"), rs.getInt("posted_by"), rs.getString("message_text"), rs.getLong("time_posted_epoch"));
+                messages.add(message);
+            }
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return messages;
+    }
+
+    public Message insertMessage(Message message){
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+//          Write SQL logic here. You should only be inserting with the name column, so that the database may
+//          automatically generate a primary key.
+            String sql = "insert into message(posted_by, message_text, time_posted_epoch) values (?, ?, ?);" ;
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            //write preparedStatement's setString method here.
+            preparedStatement.setInt(1, message.getPosted_by());
+
+            preparedStatement.setString(2, message.getMessage_text());
+
+            preparedStatement.setFloat(3, message.getTime_posted_epoch());
+
+            preparedStatement.executeUpdate();
+            ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys();
+            if(pkeyResultSet.next()){
+                int generated_message_id = (int) pkeyResultSet.getLong(1);
+                return new Message(generated_message_id, message.getPosted_by(), message.getMessage_text(), message.getTime_posted_epoch());
+            }
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
 }
