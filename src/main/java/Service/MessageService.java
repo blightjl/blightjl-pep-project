652ad21f28fe3847a813f1ee63e9cd9024520bd5
Josan Limbu <josanlimbu01@gmail.com>
2025-02-07 07:52:03 +0000

    Completed Message Service Documentation

--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -10,6 +10,9 @@ import java.util.List;
 
 /*
  * Sits between the Controller and the DAO. It calls DAO objects.
+ * 
+ * The Message Service class which contains the business logic to conducts input checks 
+ * and calls methods on the message DAO.
  */
 public class MessageService {
     private MessageDAO messageDAO;
@@ -24,6 +27,14 @@ public class MessageService {
         this.messageDAO = messageDAO;
     }
 
+    /**
+     * This method conducts input checks on message to be added such that it is in the accepted
+     * range and the account id that it is associated with exists in the database. If the message
+     * passes these checks the method calls the insert message onto the message DAO.
+     *
+     * @param message the message to be added to the database
+     * @return the added message
+     */
     public Message addMessage(Message message) {
         // check to make sure the message is in the right format
         if (message.getMessage_text().length() == 0 || message.getMessage_text().length() > 255) {
@@ -37,14 +48,34 @@ public class MessageService {
         return this.messageDAO.insertMessage(message);
     }
 
+    /**
+     * This method calls the getAllMessages() method of the message DAO to retrieve all messages.
+     *
+     * @return the list of messages returned by the message DAO.
+     */
     public List<Message> getAllMessages() {
         return this.messageDAO.getAllMessages();
     }
 
+    /**
+     * This method calls the getMessageByID() to retrieve the message that is associated with the message id.
+     *
+     * @param message the message to be added to the database
+     * @return the added message if successful, null otherwise
+     */
     public Message getMessageByID(int message_id) {
         return this.messageDAO.getMessageByID(message_id);
     }
 
+    /**
+     * This makes sure that the message text that is contained in the new message object is 
+     * within the acceptable range and that the message to be updated exists. If it passes
+     * these checks, the updateMessage() method is called on the message DAO.
+     *
+     * @param message_id the id of the message to be updated
+     * @param message the message object that contains the new message replacement
+     * @return the updated message if successful, null otherwise
+     */
     public Message updateMessage(int message_id, Message message) {
         // check to make sure the message is in the right format
         if (message.getMessage_text().length() == 0 || message.getMessage_text().length() > 255) {
@@ -59,10 +90,24 @@ public class MessageService {
         return null;
     }
 
+    /**
+     * This method calls the deleteMessage() on the messageDAO to delete a message identified by
+     * the message id parameter.
+     *
+     * @param message_id the message id of the message to be deleted
+     * @return the deleted message if successful, null otherwise
+     */
     public Message deleteMessage(int message_id) {
         return this.messageDAO.deleteMessage(message_id);
     }
 
+    /**
+     * This method calls the getAllMessagesByAccountID() on the message DAO to retrieve all
+     * messages posted by the account id.
+     *
+     * @param account_id the account it to filter the list of messages by
+     * @return a list of messages made by the account id
+     */
     public List<Message> getMessagesByAccountID(int account_id) {
         return this.messageDAO.getAllMessagesByAccountID(account_id);
     }
